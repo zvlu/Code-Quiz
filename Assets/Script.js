29100fe0f1357c19553adfbf870534c1704f0ed3
ZVLU <zvluwork@gmail.com>
2019-12-21 12:10:41 -0500

    Figure it out

--- Assets/Script.js
+++ Assets/Script.js
@@ -1,38 +1,64 @@
 const startButton = document.getElementById('start-btn')
+const headerRemove = document.getElementById('header2-container')
 const nextButton = document.getElementById('next-btn')
 const questionContainerEl = document.getElementById('game-container')
-const questionsEl = document.getElementById('question')
+const questionsEl = document.getElementById('questions')
 const answerButtonsEl = document.getElementById('answer-btns')
+
+var timerEl = document.getElementById('timer-value')
+var secondsLeft = 75;
 
 let  shuffleQuestions, currentQuestionIndex
 
 
 
 startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', nextButton)
+
 
 function startGame () {
-   
+    
     startButton.classList.add('hide')
-    shuffleQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    questionContainerEl.classList.remove('hide')
     nextButton.classList.remove('hide')
+    
+
+    shuffleQuestions = questions.sort(() => Math.random() - .5)
+    
+    currentQuestionIndex = 0
+
+    questionContainerEl.classList.remove('hide')
+  
     setNextQuestion()
+    
+    
+    
     
 }
 
-function setNextQuestion() {
+function setNextQuestion(questionnum) {
+    
     showQuestion (shuffleQuestions[currentQuestionIndex])
     
     
 }
-function showQuestion(questions) {
+function showQuestion(question) {
 
-    questionsEl.innerText = questions.question
+    questionsEl.innerText =  question.question
 
+    const buttons = document.createElement('button')
+
+    buttons.innerHTML= answers
+    
+
+
+
+    
+
+    
+
+} 
+function resetState () {
+    
 }
-
 function selectAnswer() {
 
 
--- Assets/questions.js
+++ Assets/questions.js
@@ -1,31 +1,31 @@
 //Question Variables
 
 
-var questions = [
+ questions = [
     {
         question: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+        answers: [ "strings", "booleans", "alerts", "numbers"],
+        correct: "alerts"
     },
     {
         question: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        correct: "parentheses"
     },
     {
-        question: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        question: "Which one would make this isNaN function true?",
+        answers: ["8", "curly brackets", "string", "boolean"],
+        correct: "8"
       },
     {
-        question: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        question: "What character can break JavaScript Code into several lines?",
+        answers: ["backslash", "curly brackets", "parentheses", "foward slash"],
+        correct: "backlash"
     },
     {
-        question: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        question: "What company developed JavaScript?",
+        answers: ["Netscape", "Tesla", "Java", "Google"],
+        correct: "Netscape"
     },
 
   ];
