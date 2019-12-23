9ad735a6b9847928268955506c74790d4fc75e77
ZVLU <zvluwork@gmail.com>
2019-12-22 22:44:26 -0500

    Still working on it

--- Assets/Script.js
+++ Assets/Script.js
@@ -7,60 +7,68 @@ const answerButtonsEl = document.getElementById('answer-btns')
 
 var timerEl = document.getElementById('timer-value')
 var secondsLeft = 75;
+var secondsElasped = 0;
+const timerValueEl = document.getElementById('timer-value')
+var questionNum = questions.length
+var questionIndex = 0;
 
 let  shuffleQuestions, currentQuestionIndex
 
 
 
-startButton.addEventListener('click', startGame)
+startButton.addEventListener('click', function(){
+    if (questionIndex === 0) {
+        startButton.textContent = "NEXT";
+        questionContainerEl.classList.remove('hide')
+       
+    }
 
-
-function startGame () {
-    
-    startButton.classList.add('hide')
-    nextButton.classList.remove('hide')
-    
-
-    shuffleQuestions = questions.sort(() => Math.random() - .5)
-    
-    currentQuestionIndex = 0
-
-    questionContainerEl.classList.remove('hide')
-  
-    setNextQuestion()
-    
-    
-    
-    
-}
-
-function setNextQuestion(questionnum) {
-    
-    showQuestion (shuffleQuestions[currentQuestionIndex])
-    
-    
-}
-function showQuestion(question) {
-
-    questionsEl.innerText =  question.question
-
-    const buttons = document.createElement('button')
-
-    buttons.innerHTML= answers
-    
+    renderQuestion()
+    function startTimer () {
+    setInterval(() => {
+        secondsLeft--;
+        renderTime();
+    },1000);
+    }     
+})
 
 
 
-    
+function renderQuestion () {
 
-    
+    var question = questions[questionIndex]
+    questionsEl.textContent = question.question;
 
-} 
-function resetState () {
-    
-}
-function selectAnswer() {
+    var answers = question.answers
 
+    for (var i =0; i < answers.length; i++){
+       const button = document.createElement('button')
+       button.innerText = answers [i];
+       button.className = 'answer-btn'
+       answerButtonsEl.appendChild(button)
+       
+      
+   }
+        
 
 }
+//Answers (if right answer chosen go to next question)
+answerButtonsEl.addEventListener('click', function(e){
+    var clickbtn = e.target
+    
+    var btntext = clickbtn.innerText; 
+    
+    if (btntext === questions[questionIndex].correct){
+        
+        
+        questionIndex ++;
+        this.childNode.removeParent('answer-btns');
+        renderQuestion();
+        
+        
+        
+    }
+   
+    
+})
 
